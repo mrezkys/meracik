a95eec6e6d5a4f37ef90e03a26f90e2d133dd2fa
mrezkys <mrezkysulihin@gmail.com>
2021-07-14 18:16:47 +0800

    create custom app bar

--- lib/view/widget/custom_app_bar.dart
+++ lib/view/widget/custom_app_bar.dart
@@ -1,16 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
   @override
-  Size get preferredSize => const Size.fromHeight(100);
+  Size get preferredSize => const Size.fromHeight(65);
 
   @override
   Widget build(BuildContext context) {
-    return PreferredSize(
-      preferredSize: Size.fromHeight(100),
-      child: AppBar(
-        title: Text('hello'),
-      ),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        AppBar(
+          elevation: 0.0,
+          backgroundColor: Colors.white,
+          leading: IconButton(
+            icon: SvgPicture.asset('assets/icons/menu.svg'),
+            onPressed: () {
+              Scaffold.of(context).openDrawer();
+            },
+          ),
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                'Hello, Olav',
+                style: TextStyle(color: Colors.black, fontSize: 22),
+              ),
+              Container(
+                width: 40,
+                height: 40,
+                decoration: BoxDecoration(borderRadius: BorderRadius.circular(100), color: Colors.grey),
+                child: Image.asset(
+                  'assets/images/profile.png',
+                  fit: BoxFit.cover,
+                ),
+              )
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
