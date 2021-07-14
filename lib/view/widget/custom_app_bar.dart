a052e99bf023cb721ac01d1958ba12f404a6dc9c
mrezkys <mrezkysulihin@gmail.com>
2021-07-14 18:41:45 +0800

    make parameter for custom app bar

--- lib/view/widget/custom_app_bar.dart
+++ lib/view/widget/custom_app_bar.dart
@@ -2,6 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
+  final String title;
+  final Widget leading;
+  final Widget image;
+
+  CustomAppBar({@required this.title, @required this.leading, this.image});
   @override
   Size get preferredSize => const Size.fromHeight(65);
 
@@ -14,7 +19,7 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
           elevation: 0.0,
           backgroundColor: Colors.white,
           leading: IconButton(
-            icon: SvgPicture.asset('assets/icons/menu.svg'),
+            icon: leading,
             onPressed: () {
               Scaffold.of(context).openDrawer();
             },
@@ -23,18 +28,17 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               Text(
-                'Hello, Olav',
+                title,
                 style: TextStyle(color: Colors.black, fontSize: 22),
               ),
-              Container(
-                width: 40,
-                height: 40,
-                decoration: BoxDecoration(borderRadius: BorderRadius.circular(100), color: Colors.grey),
-                child: Image.asset(
-                  'assets/images/profile.png',
-                  fit: BoxFit.cover,
-                ),
-              )
+              (image != null)
+                  ? Container(
+                      width: 40,
+                      height: 40,
+                      decoration: BoxDecoration(borderRadius: BorderRadius.circular(100), color: Colors.grey),
+                      child: image,
+                    )
+                  : SizedBox(),
             ],
           ),
         ),
